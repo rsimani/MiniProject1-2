6a6b0c311c29029887f0175d784e4e479d388a34
rivkafims148 <rivkafims148@gmail.com>
2021-05-01 22:38:40 +0300

    5

---
+++ src/unittests/ImageWriterTest.java
@@ -0,0 +1,37 @@
+package unittests;
+import renderer.ImageWriter;
+import java.awt.*;
+import org.junit.Test;
+
+import primitives.Color;
+/**
+ * Testing ImageWriter
+ * @author Rivka
+ *
+ */
+
+public class ImageWriterTest {
+	@Test
+    public void ImageWiterWriteToImageTest()
+    {
+        ImageWriter imageWriter = new ImageWriter("ImageWriterTest", 800, 500);
+  /*      , 1600, 1000*/
+        int Nx = imageWriter.getNx();
+        int Ny = imageWriter.getNy();
+        for (int i = 0; i < Ny; i++) {
+            for (int j = 0; j < Nx; j++) {
+                if (i % 50 == 0 || j % 50 == 0)
+                {
+                    imageWriter.writePixel(j, i, Color.BLACK);
+                
+                }
+                else
+                {
+                    imageWriter.writePixel(j, i, Color.BLUE);
+                }
+            }
+        }
+        imageWriter.writeToImage();
+    }
+
+}
